00476061f0e79ce2341565b3a747db060ef68e4d
lorenzobellino <lor.bellino@gmail.com>
2023-04-03 01:00:17 +0200

    added utils to cerrectly display images

--- src/utils/utils.py
+++ src/utils/utils.py
@@ -8,6 +8,30 @@ import utils.transform as T
 from config.transform import *
 from config.baseline import *
 
+colors = [
+    [128, 64, 128],
+    [244, 35, 232],
+    [70, 70, 70],
+    [102, 102, 156],
+    [190, 153, 153],
+    [153, 153, 153],
+    [250, 170, 30],
+    [220, 220, 0],
+    [107, 142, 35],
+    [152, 251, 152],
+    [0, 130, 180],
+    [220, 20, 60],
+    [255, 0, 0],
+    [0, 0, 142],
+    [0, 0, 70],
+    [0, 60, 100],
+    [0, 80, 100],
+    [0, 0, 230],
+    [119, 11, 32],
+]
+
+label_colours = dict(zip(range(NUM_CLASSES), colors))
+
 
 def setup_transform():
     transformers = []
@@ -33,6 +57,25 @@ def setup_transform():
     return transforms
 
 
+def decode_segmap(temp):
+    # convert gray scale to color
+    # print colored map
+    temp = temp.numpy()
+    r = temp.copy()
+    g = temp.copy()
+    b = temp.copy()
+    for l in range(0, NUM_CLASSES):
+        r[temp == l] = label_colours[l][0]
+        g[temp == l] = label_colours[l][1]
+        b[temp == l] = label_colours[l][2]
+
+    rgb = np.zeros((temp.shape[0], temp.shape[1], 3))
+    rgb[:, :, 0] = r / 255.0
+    rgb[:, :, 1] = g / 255.0
+    rgb[:, :, 2] = b / 255.0
+    return rgb
+
+
 def compute_moiu(net, val_dataloader):
     net = net.to(DEVICE)
     net.train(False)  # Set Network to evaluation mode
@@ -45,7 +88,6 @@ def compute_moiu(net, val_dataloader):
     for images, labels in val_dataloader:
         images = images.to(DEVICE, dtype=torch.float32)
         labels = labels.to(DEVICE, dtype=torch.long)
-
         # Forward Pass
         outputs = net(images)
         # Get predictions
@@ -57,7 +99,7 @@ def compute_moiu(net, val_dataloader):
 
     # Calculate Accuracy
     metric = jacc.item() / count
-    net.train(True)
+    # net.train(True)
     return metric
 
 
