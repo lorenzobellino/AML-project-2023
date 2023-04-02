a16c7f6b5d2b42f562c9c4a09339cb1581a3ca6b
lorenzobellino <lor.bellino@gmail.com>
2023-04-02 23:20:03 +0200

    configuration files

---
+++ src/config/federated.py
@@ -0,0 +1,13 @@
+PARTITION = "B"  # 'A' or 'B'
+SPLIT = 1  # 1 or 2 // 1 = Uniform : 2 = Heterogenous
+MAX_SAMPLE_PER_CLIENT = 20
+
+IMAGES_FINAL = "leftImg8bit"
+TARGET_FINAL = "gtFine_labelIds"
+
+N_ROUND = 50
+CLIENT_PER_ROUND = 5  # clients picked each round
+NUM_EPOCHS = 2
+
+
+CHECKPOINTS = 5
